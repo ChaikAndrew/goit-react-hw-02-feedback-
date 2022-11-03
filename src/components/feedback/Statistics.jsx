b2da20c995726bff7f2522f3a3adb4037f74ca27
ChaikAndrew <101956196+ChaikAndrew@users.noreply.github.com>
2022-11-03 09:26:42 +0100

    add styles

--- src/components/feedback/Statistics.jsx
+++ src/components/feedback/Statistics.jsx
@@ -9,11 +9,13 @@ const Statistics = ({
   PositiveFeedbackPercentage,
 }) => (
   <ul className={s.Feedback__counter}>
-    <li>Good: {goodFeedback}</li>
-    <li>Neutral: {neutralFeedback}</li>
-    <li>Bad: {badFeedback}</li>
-    <li>Total: {TotalFeedback}</li>
-    <li>Positive feedback: {PositiveFeedbackPercentage}%</li>
+    <li className={s.Feedback__list}>Good: {goodFeedback}</li>
+    <li className={s.Feedback__list}>Neutral: {neutralFeedback}</li>
+    <li className={s.Feedback__list}>Bad: {badFeedback}</li>
+    <li className={s.Feedback__list}>Total: {TotalFeedback}</li>
+    <li className={s.Feedback__list}>
+      Positive feedback: {PositiveFeedbackPercentage}%
+    </li>
   </ul>
 );
 
